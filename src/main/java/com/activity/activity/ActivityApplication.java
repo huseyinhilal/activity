3c74bc68cc4203aaf1f84d2e7c257d9c875af16e
huseyinhilal <huseyinhilall@gmail.com>
2020-05-07 18:52:34 +0300

    members tablodan veri çekiyor

--- src/main/java/com/activity/activity/ActivityApplication.java
+++ src/main/java/com/activity/activity/ActivityApplication.java
@@ -3,11 +3,32 @@ package com.activity.activity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+
 @SpringBootApplication
 public class ActivityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActivityApplication.class, args);
+
+		String connectionUrl =
+				"jdbc:sqlserver://DESKTOP-G1UMMVA\\SQLEXPRESS;databaseName=EtkinlikDb;integratedSecurity=true";
+
+		try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+			Statement stmt = connection.createStatement();
+			String SQL = "SELECT  * FROM Member";
+			ResultSet rs = stmt.executeQuery(SQL);
+
+			while (rs.next()) {
+				System.out.println(rs.getString("Member_Id") + " " + rs.getString("Name"));
+			}
+// Code here.
+			String a="";
+		}
+// Handle any errors that may have occurred.
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
